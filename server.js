46476b2245530c34a1cf97e1a2de6257e6c1361e
krishane <shaner261996@gmail.com>
2017-02-02 14:12:34 -0600

    Simplified gallery

--- server.js
+++ server.js
@@ -7,20 +7,48 @@ var stylesheet = fs.readFileSync('gallery.css');
 
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg'];
 
-function serverImage(filename, req, res){
+function getImageNames(callback){
+	fs.readdir('images/', function(err, fileNames){
+		if(err){
+			callback(err, undefined);
+		}else{
+			callback(false, fileNames);
+		}
 
-	var body = fs.readFile('images/'+filename, function(err, body){
-	if(err){
-		console.error(err);
-		res.statusCode = 500;
-		res.statusMessage = "whoops";
-		res.end("Silly me");
+	});
 
-		return;
 
-	}
-	res.setHeader("Content-Type", "image/jpeg");
-        res.end(body);
+
+
+}
+
+function imageNamesToTags(fileNames)
+{
+	console.log(fileNames);
+	return fileNames.map(function(fileName)
+	{
+		return `<img src="${fileName}" alt="${fileName}">`
+	});
+
+
+}
+
+function serveGallery(req, res){
+	getImageNames(function(err, imageNames){
+		if(err){
+
+				console.error(err);
+				res.statusCode = 500;
+				res.statusMessage = 'Server error';
+				res.end();
+				return;
+
+
+		}
+
+		res.setHeader('Content-Type', 'text/html');
+		res.end(buildGallery(imageNames));
+
 
 
 
@@ -30,46 +58,73 @@ function serverImage(filename, req, res){
 
 
 }
-var server = http.createServer((req, res) => {
-	switch(req.url){
-		case "/gallery":
-				var gHtml = imageNames.map(function(fileName){
-					return '<image src="'+fileName+'" alt="a fishing ace at work">'
 
-				}).join(' ');
-				var html ='<!doctype html>';
-						html += '<head>';
-						html += '<title>Dynamic Page</title>';
-						html += '<link href="gallery.css" rel="stylesheet" type="text/css">';
-						html += '</head>';
-						html += '<h1>Hello. </h1> Time is '+Date.now();
-						html += '<body>';
-						html += '<h1>Gallery</h1>';
-						html += '<image src="ace.jpg" alt="a fishing ace at work">';
-						html += '</body>';
-						res.setHeader('Content-Type', 'text/html');
-						res.end(html);
+function buildGallery(imageTags){
+
+	var html ='<!doctype html>';
+			html += '<head>';
+			html += '<title>Dynamic Page</title>';
+			html += '<link href="gallery.css" rel="stylesheet" type="text/css">';
+			html += '</head>';
+			html += '<h1>Hello. </h1> Time is '+Date.now();
+			html += '<body>';
+			html += '<h1>Gallery</h1>';
+			html += imageNamesToTags(imageTags).join(' ');
+			html += '</body>';
+
+	return html;
+
+
+
+
+
+}
+
+function serveImage(fileName, req, res){
+
+	var body = fs.readFile('images/'+fileName, function(err, body){
+	if(err){
+		console.error(err);
+		res.statusCode = 404;
+		res.statusMessage = "Resource not found";
+		res.end();
+
+		return;
+
+		}
+			res.setHeader("Content-Type", "image/jpeg");
+      res.end(body);
+
+
+
+	});
+
+
+
+
+}
+var server = http.createServer(function(req, res){
+	switch(req.url){
+		case '/':
+		case '/gallery':
+  		serveGallery(req, res);
+
+
+
 				break;
-		case "/chess":
-			serveImage("chess.jpg", req, res);
-			break;
-		case "/fern":
-		case "/fern/":
-		case "/fern.jpg":
-		case "/fern.jpg/":
-			serverImage("fern.jpg", req, res);
-                        break;
-		case '/ace.jpg':
-			serverImage('ace.jpg', req, res);
-			break;
+
 		case '/gallery.css':
+		case '/gallery.css/':
+		case '/gallery':
+		case '/gallery/':
 		  res.setHeader('Content-Type', 'text/css');
 			res.end(stylesheet);
 			break;
+
 		default:
-			res.statusCode = 404;
-			res.statusMessage = "Not Found";
-			res.end();
+			serveImage(req.url, req, res);
+
+			break;
 	}
 });
 

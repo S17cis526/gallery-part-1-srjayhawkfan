b1b078ca2d9d5447a94bcd30c2b9522d89078c70
krishane <shaner261996@gmail.com>
2017-02-07 14:23:34 -0600

    Added file upload

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
 "use strict"; //turns on strict node
 var http = require('http');
+var url = require('url');
 var port = 3438;
 var fs= require('fs');
 var stylesheet = fs.readFileSync('gallery.css');
-
+var config = JSON.parse(fs.readFileSync('config.json'));
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg'];
-
+var title;
 function getImageNames(callback){
 	fs.readdir('images/', function(err, fileNames){
 		if(err){
@@ -63,13 +64,21 @@ function buildGallery(imageTags){
 
 	var html ='<!doctype html>';
 			html += '<head>';
-			html += '<title>Dynamic Page</title>';
+			html += '<title>'+config.title+'</title>';
 			html += '<link href="gallery.css" rel="stylesheet" type="text/css">';
 			html += '</head>';
 			html += '<h1>Hello. </h1> Time is '+Date.now();
 			html += '<body>';
-			html += '<h1>Gallery</h1>';
+			html += '<h1>'+config.title+'</h1>';
+			html += '<form>';
+					html += '<input type="text" name="title">'
+					html += '<input type="submit" name="submit">'
+			html += '</form>'
 			html += imageNamesToTags(imageTags).join(' ');
+			html += '<form method="POST">';
+						html += '<input type="file" name="image">';
+									html += '<input type="submit" value="Upload Image">';
+			html += '</form>'	;
 			html += '</body>';
 
 	return html;
@@ -78,6 +87,35 @@ function buildGallery(imageTags){
 
 
 
+}
+
+function uploadImage(req, res){
+	var body;
+
+	req.on('error', function(){
+		res.statusCode = 500;
+		res.end();
+
+	});
+
+	req.on('data', function(data){
+		body += data;
+	});
+
+	req.on('end', function(){
+		// process information
+		// write to a fileName
+		fs.writeFile('filename', data, function(err){
+				if(err){
+					console.error(err);
+					res.statusCode = 500;
+					res.end();
+					return;
+				}
+			serveGallery(req, res)
+		})
+
+	});
 }
 
 function serveImage(fileName, req, res){
@@ -104,11 +142,30 @@ function serveImage(fileName, req, res){
 
 }
 var server = http.createServer(function(req, res){
-	switch(req.url){
+
+	var urlParts = url.parse(req.url);
+
+
+	if(urlParts.query){
+	 	var matches =	/title=(.+)(&|$)/.exec(urlParts.query);
+	 if(matches && matches[1]){
+		 title = decodeURIComponent(matches[1]);
+		 fs.writeFile('config.json', JSON.stringify({title: title}));
+
+	 }
+	}
+
+	switch(urlParts.pathname){
 		case '/':
 		case '/gallery':
   		serveGallery(req, res);
+			if(req.method == 'GET'){
+				serveGallery(req, res);
 
+			}else if(req.method == 'POST'){
+
+
+			}
 
 
 				break;
